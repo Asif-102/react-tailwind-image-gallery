67ee19887b3e3e657a6e9035b062baf89ddd9025
Asif2535 <aiqbal517@gmail.com>
2021-06-09 02:26:35 +0600

    used image api and created image card component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,33 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
+import ImageCard from './components/ImageCard';
 
 function App() {
-  
+
+  const [images, setImages] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
+  const [term, setTerm] = useState('');
+
+  const API_KEY = '8761127-15c354fd40a23de8d36bfe25d';
+
+  useEffect(()=>{
+    fetch(`https://pixabay.com/api/?key=${API_KEY}&q=${term}&image_type=photo&pretty=true`)
+      .then(res => res.json())
+      .then(data => {
+        setImages(data.hits);
+        setIsLoading(false);
+      })
+      .catch(err => console.log(err))
+  },[]);
+
   return (
-    <div>
-      <h1>Welcome</h1>
+    <div className="container mx-auto">
+      <div className="grid grid-cols-3 gap-4">
+        {
+          images.map(image=>(
+            <ImageCard key={image.id} image={image}/>
+          ))
+        }
+      </div>
     </div>
   );
 }
